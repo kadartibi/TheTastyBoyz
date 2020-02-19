bb42c8ba452a876fc885c205de8e7398bd85dd6b
Armin Kovacs <armin0191@gmail.com>
2020-02-19 15:21:26 +0100

    Create js file

--- src/components/RecommendedRecipe.js
+++ src/components/RecommendedRecipe.js
@@ -1,8 +1,13 @@
 import React, { useContext } from "react";
 import { RecommendedRecipeContext } from "./context/RecommendedRecipeContext";
+import { DisplayRecipes } from "./DisplayRecipes";
 
 export default function RecommendedRecipe() {
   const [recommendedRecipes] = useContext(RecommendedRecipeContext);
-  console.log(recommendedRecipes)
-  return <h1>Recommended Recipes</h1>;
+  console.log(recommendedRecipes);
+  return (
+    <div>
+      <DisplayRecipes></DisplayRecipes>
+    </div>
+  );
 }
